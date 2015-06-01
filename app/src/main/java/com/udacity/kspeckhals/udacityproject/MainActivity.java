3966b07dce26b56fd3c5a05ba3d97be8e52e3fd0
root <root@ip-10-47-174-141.ec2.internal>
2015-06-01 13:29:39 +0000

    Committed by Udacity

---
+++ app/src/main/java/com/udacity/kspeckhals/udacityproject/MainActivity.java
@@ -0,0 +1,102 @@
+package com.udacity.kspeckhals.udacityproject;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessageCapstone(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessageSpotify(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my spotify app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessageScores(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my scores app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessageLibrary(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my library app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessageBuildItBigger(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my build it bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessageXYZReader(View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my xyz reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
